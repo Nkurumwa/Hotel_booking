7cdb6998e733dd666f97bfc223c5f1d282b5acf1
Nkurumwa <nkurumwa51@gmail.com>
2019-12-06 11:19:14 +0300

    modifies app urls

--- Bookings/urls.py
+++ Bookings/urls.py
@@ -5,14 +5,14 @@ from . import views
 
 app_name='Bookings'
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('login', views.login, name='login'),
-    path('register', views.register, name='register'),
-    path('registration', views.registration, name='registration'),
-    path('dashboard', views.dashboard, name='dashboard'),
-    path('bookHotel', views.bookHotel, name='bookHotel'),
-    path('bookingResult', views.bookingResult, name='bookingResult'),
-    path('bookingConfirmation', views.bookingConfirmation, name='bookingConfirmation'),
-    path('viewVisits', views.viewVisits, name='viewVisits'),
-    path('draftBooking', views.draftBooking, name='draftBooking'),
+    path('',views.index,name='index'),
+    path('login/',views.login,name='login'),
+    path('register/',views.register,name='register'),
+    path('registration/',views.registration,name='registration'),
+    path('dashboard/',views.dashboard,name='dashboard'),
+    path('bookHotel/',views.bookHotel,name='bookHotel'),
+    path('bookingResult/',views.bookingResult,name='bookingResult'),
+    path('bookingConfirmation/',views.bookingConfirmation,name='bookingConfirmation'),
+    path('viewVisit/',views.viewVisit,name='viewVisit'),
+    path('draftBooking/',views.draftBooking,name='draftBooking'),
 ]
